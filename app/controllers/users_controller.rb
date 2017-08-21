73bd1508fb3a72142ae41fea2e028261afb71266
mikoumed <mikoumed87@gmail.com>
2017-08-21 22:45:10 +0100

    editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user, only:[:destroy]
+
+
     def new
         @user = User.new
     end
 
     def edit
+        @user = User.find(params[:id])
     end
 
     def show
@@ -13,7 +19,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
             if @user.save
-                log_in @user
+                log_in_as @user
                 flash[:success] = "Welcome to EfdmRoR!"
                 redirect_to @user # equivalent to redirect_to user_url(@user)
             else
@@ -21,10 +27,41 @@ class UsersController < ApplicationController
             end
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
     private
 
 # permit only the 4 attributes
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+# Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in"
+            redirect_to login_url
+        end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
     end
 end
