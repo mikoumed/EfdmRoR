9c73df912cbb1e5a60eaf2a87d4c7e85c7a2559c
mikoumed <mikoumed87@gmail.com>
2018-06-26 12:33:17 +0100

    sidenav class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  private
+def index
+    @issues = current_team.issues
+    @today_issues = @issues.where("end >=  ? and start <= ?", Time.now , Time.now.end_of_day).count
+end
+
+private
 
   # Confirms a logged-in user.
   def logged_in_user
@@ -12,5 +17,10 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+
+  # Returns the team of the current user
+  	def current_team
+  		Team.find_by(id: current_user.team_id)
+  	end
 
 end
